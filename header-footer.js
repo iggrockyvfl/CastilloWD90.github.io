7cdb9a03eb63902085fe6af98b29ed8b43276430
iggrockyvfl <iggrockyvfl@gmail.com>
2023-10-19 13:48:12 +0800

    Update header-footer.js

--- header-footer.js
+++ header-footer.js
@@ -132,7 +132,7 @@ class MyHeader extends HTMLElement {
         <body>
         <nav class="navbar navbar-expand-lg">
         <div class="container-fluid">
-          <a class="navbar-brand" href="#"><img src="120px-Architectural_Association_School_of_Architecture_logo.png" id="logo"></a>
+          <a class="navbar-brand" href="index.html"><img src="120px-Architectural_Association_School_of_Architecture_logo.png" id="logo"></a>
           <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
           </button>
